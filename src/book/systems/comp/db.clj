fb4470736ce61f94dec56d28f2857743e53d8e3f
Ivan Grishaev <ivan@grishaev.me>
2019-08-09 09:29:37 +0300

    drafts

--- src/book/systems/comp/db.clj
+++ src/book/systems/comp/db.clj
@@ -19,9 +19,8 @@
   component/Lifecycle
 
   (start [this]
-    (let [pool (cp/make-datasource options)
-          spec {:datasource pool}]
-      (assoc this :db-spec spec)))
+    (let [pool (cp/make-datasource options)]
+      (assoc this :db-spec {:datasource pool})))
 
   (stop [this]
     (-> db-spec :datasource cp/close-datasource)
@@ -36,8 +35,8 @@
     (jdbc/update! db-spec table set-map where-clause)))
 
 
-(defn make-db [pool-options]
-  (map->DB {:options pool-options}))
+(defn make-db [options]
+  (map->DB {:options options}))
 
 
 (defmacro with-db-transaction
@@ -47,3 +46,90 @@
        [t-conn# db-spec# ~@trx-opt]
        (let [~comp-trx (assoc ~comp-db :db-spec t-conn#)]
          ~@body))))
+
+
+
+#_
+(defrecord DB
+    [options db-spec]
+  component/Lifecycle
+  (start [this]
+    (let [pool (cp/make-datasource options)]
+      (assoc this :db-spec {:datasource pool})))
+  (stop [this]
+    (-> db-spec :datasource cp/close-datasource)
+    (assoc this :db-spec nil)))
+
+
+
+#_
+(defn ___ []
+  (let [db-started (-> {} make-db component/start)
+        {:keys [db-spec]} db-started
+        users (jdbc/query db-spec "select * from users")]
+    (do users)))
+
+#_
+(let [{:keys [db-spec]} db-started
+      users (jdbc/query db-spec "select * from users")]
+  (process-users users))
+
+
+#_
+(;; ........
+
+  (stop [this]
+    (-> db-spec :datasource cp/close-datasource)
+      (assoc this :db-spec nil))
+
+  IDB
+
+  (query [this sql-params]
+    (jdbc/query db-spec sql-params))
+
+  (update! [this table set-map where-clause]
+    (jdbc/update! db-spec table set-map where-clause)))
+
+
+#_
+(def options
+  {:auto-commit        true
+   :read-only          false
+   :connection-timeout 30000
+   :validation-timeout 5000
+   :idle-timeout       600000
+   :max-lifetime       1800000
+   :minimum-idle       10
+   :maximum-pool-size  10
+   :pool-name          "book-pool"
+   :adapter            "postgresql"
+   :username           "book"
+   :password           "book"
+   :database-name      "book"
+   :server-name        "127.0.0.1"
+   :port-number        5432
+   :register-mbeans    false})
+
+
+;; (def db-created (make-db options))
+;; (def db-started (component/start db-created))
+;; (query db-started "select * from requests")
+;; (update! db-started :requests {:is_processed false} ["id = ?" 42])
+;; (def db-stopped (component/stop db-started))
+
+
+#_
+(with-db-transaction
+  [db-tx db-started]
+  (let [requests (query db-tx "select * from requests limit 1 for update")]
+    (when-let [request (first requests)]
+      (update! db-tx :requests {:is_processed false} ["id = ?" (:id request)]))))
+
+
+"
+BEGIN
+select * from requests limit 1 for update
+UPDATE requests SET is_processed = $1 WHERE id = $2
+DETAIL:  parameters: $1 = 'f', $2 = '3'
+COMMIT
+"
--- src/book/systems/comp/server.clj
+++ src/book/systems/comp/server.clj
@@ -19,11 +19,73 @@
     (.stop server)
     (assoc this :server nil)))
 
+(defn make-server
+  [options]
+  (map->Server {:options options}))
 
 ;; (def c0 (map->Server {:options {:port 8080 :join? false}}))
 ;; (def c1 (component/start c0))
 ;; (def c2 (component/stop c1))
 
-(defn make-server
-  [jetty-options]
-  (map->Server {:options jetty-options}))
+;; (def s-created (map->Server {:options {:port 8080 :join? false}}))
+;; (def s-started (component/start s-created))
+;; (def s-stopped (component/stop s-started))
+
+#_
+(def s-created
+  (map->Server
+   {:options {:port 8080 :join? false}}))
+
+#_(def s-started (component/start s-created))
+
+#_(def s-stopped (component/stop s-started))
+
+#_
+(def s-created (make-server {:port 8080 :join? false}))
+
+#_
+(defrecord Server
+    [options
+     server]
+
+    component/Lifecycle
+
+    (start [this]
+      (if server
+        this
+        (let [server (run-jetty app options)]
+          (assoc this :server server))))
+
+    (stop [this]
+      (when server
+        (.stop server))
+      (assoc this :server nil)))
+
+
+
+(defrecord BadServer
+  [options server]
+  component/Lifecycle
+  (start [this]
+    {:server (run-jetty app options)})
+  (stop [this]
+    (.stop server)
+    nil))
+
+
+(def bs-created (map->BadServer {:options {:port 8080 :join? false}}))
+(def bs-started (component/start bs-created))
+
+"
+2019-08-07 10:21:47,531 INFO  org.eclipse.jetty.server.Server - jetty-9.4.12.v20180830; built: 2018-08-30T13:59:14.071Z; git: 27208684755d94a92186989f695db2d7b21ebc51; jvm 1.8.0_102-b14
+2019-08-07 10:21:47,538 INFO  o.e.jetty.server.AbstractConnector - Started ServerConnector@405ff5ed{HTTP/1.1,[http/1.1]}{0.0.0.0:8080}
+2019-08-07 10:21:47,542 INFO  org.eclipse.jetty.server.Server - Started @61386534ms
+"
+
+(def bs-stopped (component/stop bs-started))
+
+#_
+{:server #object[org.eclipse.jetty.server.Server 0x410a0712 "Server@410a0712{STARTED}[9.4.12.v20180830]"]}
+
+#_
+NullPointerException
