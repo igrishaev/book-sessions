4809c35e78859d6bf9149bde4fc24b10d76e4729
Ivan Grishaev <ivan@grishaev.me>
2022-04-17 18:06:58 +0300

    updated

--- repl/samples.clj
+++ repl/samples.clj
@@ -11,3 +11,20 @@
 
 
 ;; clj -R:nrepl -m nrepl.cmdline --middleware "[cider.piggieback/wrap-cljs-repl]"
+
+
+fs.readdirSync('./dirpath', {withFileTypes: true})
+.filter(item => !item.isDirectory())
+.map(item => item.name)
+
+const fs = require("fs")
+
+(def js (js/require "fs"))
+
+(let [files (fs.readdirSync "/Users/ivan" #js {:withFileTypes true})]
+  (doseq [file files]
+    (println file.name)))
+
+
+clj -M -m cljs.main --repl-env node
+clj -M -m cljs.main --repl-env browser
